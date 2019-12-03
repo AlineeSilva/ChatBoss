bad3e9c80cd8d72ee03501be2a5b9ad80dea3599
Aline Silva <aline_silva93@outlook.com.br>
2019-12-03 19:44:00 -0300

    update server.js

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ var server = require('./app');
 var port = process.env.PORT || 3000;
 
 server.listen(port, function() {
-  // eslint-disable-next-line
+  
   console.log('Server running on port: %d', port);
 });
